f34dd7440a02898b39fcd5508fd76dd7cbff93b5
azumakuniyuki <azumakuniyuki@gmail.com>
2013-02-13 14:55:53 +0900

    Support EUC-JP, ISO-2022-JP, and Shift-JIS

--- lib/Acme/Nyaa/Ja.pm
+++ lib/Acme/Nyaa/Ja.pm
@@ -3,20 +3,20 @@ use strict;
 use warnings;
 use utf8;
 use Encode;
+use Encode::Guess qw(shift-jis euc-jp 7bit-jis);;
 
 my $RxComma = qr/[、,]/;
 my $RxPeriod = qr/[。.]/;
-my $RxHiragana = qr/[あ-ん]/;
-my $RxKatakana = qr/[ア-ン]/;
 my $RxConversation = qr/[「『].+[」』]/;
 my $RxEndOfSentence = qr/[!！?？(?:...)(?:。。。)(?:,,,)(?:、、、)…]+/;
-
 my $Cats = [ '猫', 'ネコ', 'ねこ' ];
 my $HiraganaNya = 'にゃ';
 my $KatakanaNya = 'ニャ';
 my $FightingCats = [
 	'「マーオ」',
+	'「マーオ!」',
 	'「マーーオ」',
+	'「マーーオ!」',
 	'「マーーーオ!!」',
 	'「マーーーーオ!!!」',
 ];
@@ -39,23 +39,24 @@ sub cat
 {
 	my $class = shift;
 	my $text0 = shift;
+	my $text1 = undef;
 	my $bless = ref $text0;
 
 	return q() if( $bless ne '' && $bless ne 'SCALAR' );
-	my $text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
+	$text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
 	return q() unless length $text1;
+
+	my $cname = __PACKAGE__->_reckon( \$text1 );
+	my $uflag = $cname eq 'utf8' ? utf8::is_utf8 $text1 : undef;
+
+	return $text1 unless $cname;
+	$text1 =  __PACKAGE__->_toutf8( $text1, $cname, $uflag );
+	$text1 =~ s{($RxPeriod)}{$1\x1f}g;
 
 	my $sizeh = scalar @$HiraganaTails;
 	my $sizek = scalar @$KatakanaTails;
 	my $index = 0;
-	my $lines = [ split( $RxPeriod, $text1 ) ];
-
-	if( $text1 =~ m/($RxPeriod)/ )
-	{ 
-		# Perl::Critic found these violations in "lib/Acme/Nyaa/Ja.pm":
-		# Don't modify $_ in list functions at line 61, column 2.  See page 114 of PBP.  (Severity: 5)
-		foreach my $e ( @$lines ){ $e .= $1; }
-	}
+	my $lines = [ split( /\x1F/, $text1 ) ];
 
 	foreach my $e ( @$lines )
 	{
@@ -63,7 +64,12 @@ sub cat
 		next if grep { $e eq $_ } @$DoNotBecomeCat;
 		next if grep { $e =~ m/$_$RxPeriod?\z/ } @$HiraganaTails;
 		next if grep { $e =~ m/$_$RxPeriod?\z/ } @$KatakanaTails;
+		next if grep { $e =~ m/$_$RxEndOfSentence?\z/ } @$HiraganaTails;
+		next if grep { $e =~ m/$_$RxEndOfSentence?\z/ } @$KatakanaTails;
+		next if grep { $e =~ m/$_\z/ } @$FightingCats;
+
 		next if $e =~ m{\A[\x20-\x7E]+\z};
+		next unless $e =~ m{[\p{InHiragana}\p{InKatakana}]+};
 
 		if( $e =~ m/な($RxPeriod?)\z/ )
 		{
@@ -75,12 +81,12 @@ sub cat
 			# ナ => ニャー
 			$e =~ s/ナ($RxPeriod?)\z/$HiraganaNya$1/;
 		}
-		elsif( $e =~ m/$RxHiragana$RxPeriod\z/ )
+		elsif( $e =~ m/\p{InHiragana}$RxPeriod\z/ )
 		{
 			$index = int rand $sizek;
 			$e =~ s/($RxPeriod)\z/$KatakanaTails->[ $index ]$1/;
 		}
-		elsif( $e =~ m/$RxKatakana$RxPeriod\z/ )
+		elsif( $e =~ m/\p{InKatakana}$RxPeriod\z/ )
 		{
 			$index = int rand $sizeh;
 			$e =~ s/($RxPeriod)\z/$HiraganaTails->[ $index ]$1/;
@@ -91,7 +97,7 @@ sub cat
 			{
 				# ... => ニャー..., ! => ニャ!
 				my $eos = $1;
-				if( $e =~ m/$RxKatakana$RxEndOfSentence\z/ )
+				if( $e =~ m/\p{InKatakana}$RxEndOfSentence\z/ )
 				{
 					$index = int rand( $sizeh / 2 );
 					$e =~ s/$RxEndOfSentence/$HiraganaTails->[ $index ]$eos/g;
@@ -121,19 +127,25 @@ sub cat
 		}
 	}
 
-	return join( '', @$lines );
+	return __PACKAGE__->_utf8to( join( '', @$lines ), $cname, $uflag );
 }
 
 sub neko
 {
 	my $class = shift;
 	my $text0 = shift;
+	my $text1 = undef;
 	my $bless = ref $text0;
 
 	return q() if( $bless ne '' && $bless ne 'SCALAR' );
-	my $text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
-	my $sizec = scalar @$Cats;
+	$text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
 	return q() unless length $text1;
+
+	my $cname = __PACKAGE__->_reckon( \$text1 );
+	my $uflag = $cname eq 'utf8' ? utf8::is_utf8 $text1 : undef;
+
+	return $text1 unless $cname;
+	$text1 = __PACKAGE__->_toutf8( $text1, $cname, $uflag );
 
 	my $map = {
 		'神' => 'ネコ',
@@ -145,17 +157,59 @@ sub neko
 		my $f = $map->{ $e };
 
 		$text1 =~ s{\A[$e]\z}{$f};
-		$text1 =~ s{\A[$e]($RxHiragana)}{$f$1};
-		$text1 =~ s{\A[$e]($RxKatakana)}{$f$1};
-		$text1 =~ s{($RxHiragana)[$e]($RxHiragana)}{$1$f$2}g;
-		$text1 =~ s{($RxHiragana)[$e]($RxKatakana)}{$1$f$2}g;
-		$text1 =~ s{($RxKatakana)[$e]($RxKatakana)}{$1$f$2}g;
-		$text1 =~ s{($RxKatakana)[$e]($RxHiragana)}{$1$f$2}g;
-		$text1 =~ s{($RxHiragana)[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
-		$text1 =~ s{($RxKatakana)[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
+		$text1 =~ s{\A[$e](\p{InHiragana})}{$f$1};
+		$text1 =~ s{\A[$e](\p{InKatakana})}{$f$1};
+		$text1 =~ s{(\p{InHiragana})[$e](\p{InHiragana})}{$1$f$2}g;
+		$text1 =~ s{(\p{InHiragana})[$e](\p{InKatakana})}{$1$f$2}g;
+		$text1 =~ s{(\p{InKatakana})[$e](\p{InKatakana})}{$1$f$2}g;
+		$text1 =~ s{(\p{InKatakana})[$e](\p{InHiragana})}{$1$f$2}g;
+		$text1 =~ s{(\p{InHiragana})[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
+		$text1 =~ s{(\p{InKatakana})[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
 	}
 
-	return $text1;
+	return __PACKAGE__->_utf8to( $text1, $cname, $uflag );
+}
+
+sub _reckon
+{
+	my $class = shift;
+	my $text0 = shift;
+	my $bless = ref $text0;
+
+	my $text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
+	return q() unless length $text1;
+
+	my $guess = Encode::Guess->guess( $text1 );
+	return q() unless ref $guess;
+	return $guess->name;
+}
+
+sub _toutf8
+{
+	my $class = shift;
+	my $text0 = shift // return q();
+	my $cname = shift || __PACKAGE__->_reckon( \$text0 );
+	my $uflag = shift // 0;
+
+	return $text0 unless $cname;
+	Encode::from_to( $text0, $cname, 'utf8' ) if $cname ne 'utf8';
+	$uflag = utf8::is_utf8($text0);
+	utf8::decode $text0 unless $uflag;
+
+	return $text0;
+}
+
+sub _utf8to
+{
+	my $class = shift;
+	my $text0 = shift // return q();
+	my $cname = shift || return $text0;
+	my $uflag = shift // 0;
+
+	utf8::encode $text0 if( $uflag == 0 && utf8::is_utf8 $text0 );
+	Encode::from_to( $text0, 'utf8', $cname ) if $cname ne 'utf8';
+
+	return $text0;
 }
 
 1;
