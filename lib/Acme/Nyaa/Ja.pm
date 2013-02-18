be213a6d1b66889040e1a97b6057721dde40db39
azumakuniyuki <azumakuniyuki@gmail.com>
2013-02-18 23:18:07 +0900

    Acme::Nyaa::Ja became a sub class of Acme::Nyaa, and code refactoring

--- lib/Acme/Nyaa/Ja.pm
+++ lib/Acme/Nyaa/Ja.pm
@@ -1,4 +1,5 @@
 package Acme::Nyaa::Ja;
+use parent 'Acme::Nyaa';
 use strict;
 use warnings;
 use utf8;
@@ -45,36 +46,59 @@ my $DoNotBecomeCat = [
 	'モー娘。',
 ];
 
-sub cat
+sub new
 {
 	my $class = shift;
-	my $text0 = shift;
-	my $text1 = undef;
-	my $text2 = undef;
-	my $bless = ref $text0;
+	my $argvs = { @_ };
 
-	return q() if( $bless ne '' && $bless ne 'SCALAR' );
-	$text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
-	return q() unless length $text1;
+	return $class if ref $class eq __PACKAGE__;
+	$argvs->{'language'} = 'ja';
+	return bless $argvs, __PACKAGE__;
+}
 
-	my $cname = __PACKAGE__->_reckon( \$text1 ) || 'utf8';
-	my $uflag = $cname eq 'utf8' ? utf8::is_utf8 $text1 : undef;
+sub language
+{
+	my $self = shift;
 
-	# return $text1 unless $cname;
-	eval { $text2 =  __PACKAGE__->_toutf8( $text1, $cname, $uflag ); };
-	return $text1 if $@;
+	$self->{'language'} ||= 'ja';
+	return $self->{'language'};
+}
 
-	$text2 =~ s{($RxPeriod)}{$1$Separator}g;
-	$text2 .= $Separator unless $text2 =~ m{$Separator};
+sub object { shift }
+*objects = *object;
+*findobject = *object;
 
-	my $sizeh = scalar @$HiraganaTails;
-	my $sizek = scalar @$KatakanaTails;
-	my $index = 0;
-	my $gauge = 0;
-	my $chomp = 0;
-	my $lines = [ split( $Separator, $text2 ) ];
+sub cat
+{
+	my $self = shift;
+	my $text = shift;
 
-	foreach my $e ( @$lines )
+	my $beinputted = undef;
+	my $utf8string = undef;
+	my $referenced = ref $text;
+	my $guessedenc = undef;
+	my $wellformed = undef;
+
+	return q() if( $referenced ne '' && $referenced ne 'SCALAR' );
+	$beinputted = $referenced eq 'SCALAR' ? $$text : $text;
+	return q() unless length $beinputted;
+
+	$guessedenc = __PACKAGE__->reckon( \$beinputted ) || 'utf8';
+	$wellformed = $guessedenc eq 'utf8' ? utf8::is_utf8 $beinputted : undef;
+
+	eval { $utf8string =  __PACKAGE__->toutf8( $beinputted, $guessedenc, $wellformed ); };
+	return $beinputted if $@;
+
+	$utf8string =~ s{($RxPeriod)}{$1$Separator}g;
+	$utf8string .= $Separator unless $utf8string =~ m{$Separator};
+
+	my $hiralength = scalar @$HiraganaTails;
+	my $katalength = scalar @$KatakanaTails;
+	my $writingset = [ split( $Separator, $utf8string ) ];
+	my $haschomped = 0;
+	my ( $r1,$r2 ) = 0;
+
+	foreach my $e ( @$writingset )
 	{
 		next if $e =~ m/\A$RxPeriod\s*\z/;
 		next if $e =~ m/$RxEndOfList\s*\z/;
@@ -85,15 +109,17 @@ sub cat
 		next if grep { $e =~ m/$_$RxEndOfSentence?\s*\z/ } @$KatakanaTails;
 		next if grep { $e =~ m/$_\s*\z/ } @$FightingCats;
 
+		# Do not convert if the string contain only ASCII characters.
 		next if $e =~ m{\A[\x20-\x7E]+\z};
 
 		# ひらがな、またはカタカナが入ってないなら次へ
 		next unless $e =~ m{[\p{InHiragana}\p{InKatakana}]+};
 
+		# Cats may be hard to speak a word which ends with a character 'ね'.
 		# 「ね」の後ろにニャーがあると猫が喋りにくそう
 		next if $e =~ m{[ねネ]$RxPeriod?\s*\z};
 
-		$chomp = chomp $e;
+		$haschomped = chomp $e;
 
 		if( $e =~ m/な$RxPeriod?\s*\z/ )
 		{
@@ -107,19 +133,19 @@ sub cat
 		}
 		elsif( $e =~ m/\p{InHiragana}$RxPeriod\s*\z/ )
 		{
-			$index = int rand $sizek;
-			$e =~ s/($RxPeriod)(\s*)\z/$KatakanaTails->[ $index ]$1$2/;
+			$r1 = int rand $katalength;
+			$e =~ s/($RxPeriod)(\s*)\z/$KatakanaTails->[ $r1 ]$1$2/;
 		}
 		elsif( $e =~ m/\p{InKatakana}$RxPeriod\s*\z/ )
 		{
-			$index = int rand $sizeh;
-			$e =~ s/($RxPeriod)(\s*)\z/$HiraganaTails->[ $index ]$1$2/;
+			$r1 = int rand $hiralength;
+			$e =~ s/($RxPeriod)(\s*)\z/$HiraganaTails->[ $r1 ]$1$2/;
 		}
 		elsif( $e =~ m/\p{InCJKUnifiedIdeographs}$RxPeriod?\s*\z/ )
 		{
-			$index = int rand $sizeh;
-			$gauge = int rand scalar @$Copulae;
-			$e =~ s/($RxPeriod?)(\s*)\z/$Copulae->[ $gauge ]$KatakanaTails->[ $index ]$1$2/;
+			$r1 = int rand $hiralength;
+			$r2 = int rand scalar @$Copulae;
+			$e =~ s/($RxPeriod?)(\s*)\z/$Copulae->[ $r2 ]$KatakanaTails->[ $r1 ]$1$2/;
 		}
 		else
 		{
@@ -129,19 +155,19 @@ sub cat
 				my $eos = $1;
 				if( $e =~ m/\p{InKatakana}$RxEndOfSentence\s*\z/ )
 				{
-					$index = int rand( $sizeh / 2 );
-					$e =~ s/$RxEndOfSentence/$HiraganaTails->[ $index ]$eos/g;
+					$r1 = int rand( $hiralength / 2 );
+					$e =~ s/$RxEndOfSentence/$HiraganaTails->[ $r1 ]$eos/g;
 				}
 				elsif( $e =~ m/\p{InHiragana}$RxEndOfSentence\s*\z/ )
 				{
-					$index = int rand( $sizek / 2 );
-					$e =~ s/$RxEndOfSentence/$KatakanaTails->[ $index ]$eos/g;
+					$r1 = int rand( $katalength / 2 );
+					$e =~ s/$RxEndOfSentence/$KatakanaTails->[ $r1 ]$eos/g;
 				}
 				else
 				{
-					$index = int rand( $sizek / 2 );
-					$gauge = int rand( scalar @$Copulae );
-					$e =~ s/$RxEndOfSentence/$Copulae->[ $gauge ]$KatakanaTails->[ $index ]$eos/g;
+					$r1 = int rand( $katalength / 2 );
+					$r2 = int rand( scalar @$Copulae );
+					$e =~ s/$RxEndOfSentence/$Copulae->[ $r2 ]$KatakanaTails->[ $r1 ]$eos/g;
 				}
 			}
 			elsif( $e =~ m/$RxConversation\s*\z/ )
@@ -149,129 +175,131 @@ sub cat
 				# 0.5の確率で会話の後ろで猫が喧嘩をする
 				if( $e =~ m/\A(.*$RxConversation[ ]*)($RxConversation.*)\s*\z/ )
 				{
-					$index = int rand scalar @$FightingCats;
-					$e = $1.$FightingCats->[ $index ].$2 if int(rand(10)) % 2;
+					$r1 = int rand scalar @$FightingCats;
+					$e = $1.$FightingCats->[ $r1 ].$2 if int(rand(10)) % 2;
 				}
-				$index = int rand scalar @$FightingCats;
-				$e .= $FightingCats->[ $index ] if int(rand(10)) % 2;
+				$r1 = int rand scalar @$FightingCats;
+				$e .= $FightingCats->[ $r1 ] if int(rand(10)) % 2;
 			}
 			else
 			{
-				$index = int rand $sizek;
+				$r1 = int rand $katalength;
 
 				if( $e =~ m/[0-9\p{Latin}]\s*\z/ )
 				{
-					$gauge = int rand scalar @$Copulae;
-					$e =~ s/(\s*?)\z/ $Copulae->[ $gauge ]$KatakanaTails->[ $index ]$1/;
+					$r2 = int rand scalar @$Copulae;
+					$e =~ s/(\s*?)\z/ $Copulae->[ $r2 ]$KatakanaTails->[ $r1 ]$1/;
 				}
 				elsif( $e =~ m/\p{InKatakana}\s*\z/ )
 				{
-					$e =~ s/(\s*?)\z/$HiraganaTails->[ $index ]$1/;
+					$e =~ s/(\s*?)\z/$HiraganaTails->[ $r1 ]$1/;
 				}
 				else
 				{
-					$e =~ s/(\s*?)\z/$KatakanaTails->[ $index ]$1/;
+					$e =~ s/(\s*?)\z/$KatakanaTails->[ $r1 ]$1/;
 				}
 			}
 		}
 
-		$e .= qq(\n) if $chomp;
+		$e .= qq(\n) if $haschomped;
 
-	} # End of foreach(@$lines)
+	} # End of foreach(@$writingset)
 
-	return __PACKAGE__->_utf8to( join( '', @$lines ), $cname, $uflag );
+	return __PACKAGE__->utf8to( join( '', @$writingset ), $guessedenc, $wellformed );
 }
 
 sub neko
 {
-	my $class = shift;
-	my $text0 = shift;
-	my $text1 = undef;
-	my $text2 = undef;
-	my $bless = ref $text0;
+	my $self = shift;
+	my $text = shift;
 
-	return q() if( $bless ne '' && $bless ne 'SCALAR' );
-	$text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
-	return q() unless length $text1;
+	my $beinputted = undef;
+	my $utf8string = undef;
+	my $referenced = ref $text;
+	my $guessedenc = undef;
+	my $wellformed = undef;
+	my $nounstable = undef;
 
-	my $cname = __PACKAGE__->_reckon( \$text1 ) || 'utf8';
-	my $uflag = $cname eq 'utf8' ? utf8::is_utf8 $text1 : undef;
+	return q() if( $referenced ne '' && $referenced ne 'SCALAR' );
+	$beinputted = $referenced eq 'SCALAR' ? $$text : $text;
+	return q() unless length $beinputted;
 
-	eval { $text2 = __PACKAGE__->_toutf8( $text1, $cname, $uflag ); };
-	return $text1 if $@;
+	$guessedenc = __PACKAGE__->reckon( \$beinputted ) || 'utf8';
+	$wellformed = $guessedenc eq 'utf8' ? utf8::is_utf8 $beinputted : undef;
 
-	my $map = {
+	eval { $utf8string = __PACKAGE__->toutf8( $beinputted, $guessedenc, $wellformed ); };
+	return $beinputted if $@;
+
+	$nounstable = {
 		'神' => 'ネコ',
 	};
 
-	foreach my $e ( keys %$map )
+	foreach my $e ( keys %$nounstable )
 	{
-		next unless $text2 =~ m{$e};
-		my $f = $map->{ $e };
+		next unless $utf8string =~ m{$e};
+		my $f = $nounstable->{ $e };
 
-		$text2 =~ s{\A[$e]\z}{$f};
-		$text2 =~ s{\A[$e](\p{InHiragana})}{$f$1};
-		$text2 =~ s{\A[$e](\p{InKatakana})}{$f$1};
-		$text2 =~ s{(\p{InHiragana})[$e](\p{InHiragana})}{$1$f$2}g;
-		$text2 =~ s{(\p{InHiragana})[$e](\p{InKatakana})}{$1$f$2}g;
-		$text2 =~ s{(\p{InKatakana})[$e](\p{InKatakana})}{$1$f$2}g;
-		$text2 =~ s{(\p{InKatakana})[$e](\p{InHiragana})}{$1$f$2}g;
-		$text2 =~ s{(\p{InHiragana})[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
-		$text2 =~ s{(\p{InKatakana})[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
+		$utf8string =~ s{\A[$e]\z}{$f};
+		$utf8string =~ s{\A[$e](\p{InHiragana})}{$f$1};
+		$utf8string =~ s{\A[$e](\p{InKatakana})}{$f$1};
+		$utf8string =~ s{(\p{InHiragana})[$e](\p{InHiragana})}{$1$f$2}g;
+		$utf8string =~ s{(\p{InHiragana})[$e](\p{InKatakana})}{$1$f$2}g;
+		$utf8string =~ s{(\p{InKatakana})[$e](\p{InKatakana})}{$1$f$2}g;
+		$utf8string =~ s{(\p{InKatakana})[$e](\p{InHiragana})}{$1$f$2}g;
+		$utf8string =~ s{(\p{InHiragana})[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
+		$utf8string =~ s{(\p{InKatakana})[$e]($RxPeriod|$RxComma)?\z}{$1$f$2}g;
 	}
 
-	return __PACKAGE__->_utf8to( $text2, $cname, $uflag );
+	return __PACKAGE__->utf8to( $utf8string, $guessedenc, $wellformed );
 }
 
 sub nyaa
 {
-	my $class = shift;
-	my $sizec = 0;
-	my $nyaan = [];
+	my $self = shift;
+	my $nyaa = [];
 
-	push @$nyaan, @$KatakanaTails, @$HiraganaTails;
-	return $nyaan->[ int rand( scalar @$nyaan ) ];
+	push @$nyaa, @$KatakanaTails, @$HiraganaTails;
+	return $nyaa->[ int rand( scalar @$nyaa ) ];
 }
 
-sub _reckon
+sub reckon
 {
 	my $class = shift;
 	my $text0 = shift;
-	my $bless = ref $text0;
 
-	my $text1 = $bless eq 'SCALAR' ? $$text0 : $text0;
-	return q() unless length $text1;
+	my $referenced = ref $text0;
+	my $beinputted = $referenced eq 'SCALAR' ? $$text0 : $text0;
+	return q() unless length $beinputted;
 
-	my $guess = Encode::Guess->guess( $text1 );
-	return q() unless ref $guess;
-	return $guess->name;
+	my $guessedenc = Encode::Guess->guess( $beinputted );
+	return q() unless ref $guessedenc;
+	return $guessedenc->name;
 }
 
-sub _toutf8
+sub toutf8
 {
 	my $class = shift;
 	my $text0 = shift // return q();
-	my $cname = shift || __PACKAGE__->_reckon( \$text0 );
+	my $guess = shift || __PACKAGE__->reckon( \$text0 );
 	my $uflag = shift // 0;
 
-	return $text0 unless $cname;
-	Encode::from_to( $text0, $cname, 'utf8' ) if $cname ne 'utf8';
+	return $text0 unless $guess;
+
+	Encode::from_to( $text0, $guess, 'utf8' ) if $guess ne 'utf8';
 	$uflag = utf8::is_utf8($text0);
 	utf8::decode $text0 unless $uflag;
-
 	return $text0;
 }
 
-sub _utf8to
+sub utf8to
 {
 	my $class = shift;
 	my $text0 = shift // return q();
-	my $cname = shift || return $text0;
+	my $guess = shift || return $text0;
 	my $uflag = shift // 0;
 
 	utf8::encode $text0 if( $uflag == 0 && utf8::is_utf8 $text0 );
-	Encode::from_to( $text0, 'utf8', $cname ) if $cname ne 'utf8';
-
+	Encode::from_to( $text0, 'utf8', $guess ) if $guess ne 'utf8';
 	return $text0;
 }
 
@@ -286,43 +314,49 @@ Acme::Nyaa - Convert texts like which a cat is talking in Japanese
 
 =head1 SYNOPSIS
 
-  use Acme::Nyaa;
+	use Acme::Nyaa::Ja;
+	my $kijitora = Acme::Nyaa::Ja->new();
 
-  my $kijitora = Acme::Nyaa->new( 'language' => 'ja' );
+	# the following code is equivalent to the above.
 
-  print $kijitora->cat( \'猫がかわいい。' ); # => 猫がかわいいニャー。
-  print $kijitora->neko( \'神と和解せよ' ); # => ネコと和解せよ
+	use Acme::Nyaa;
+	my $kijitora = Acme::Nyaa->new( 'language' => 'ja' );
 
+
+	print $kijitora->cat( \'猫がかわいい。' );	# => 猫がかわいいニャー。
+	print $kijitora->neko( \'神と和解せよ' );	# => ネコと和解せよ
 
 =head1 DESCRIPTION
   
-  Acme::Nyaa is a converter which translate Japanese texts to texts
-  like which a cat talking.
+Acme::Nyaa is a converter which translate Japanese texts to texts like which a cat talking.
+Language modules are available only Japanese (L<Acme::Nyaa::Ja>) for now.
 
-=head1 METHODS
+=head1 CLASS METHODS
 
-=over
+=head2 B<new>
 
-=item new
-  new() is a constructor of Acme::Nyaa
+new() is a constructor of Acme::Nyaa::Ja
 
-=item cat
-  cat() is a converter that appends string "ニャー" at the end of
-  each sentence.
+=head1 INSTANCE METHODS
 
-=item neko
-  neko() is a converter that replace a noun with 'ネコ'.
+=head2 B<cat>
 
-=item nyaa
-  nyaa() returns string: 'ニャー'.
+cat() is a converter that appends string C<ニャー> at the end of each sentence.
 
-=back
+=head2 B<neko>
+
+neko() is a converter that replace a noun with C<ネコ>.
+
+=head2 B<nyaa>
+
+nyaa() returns string: C<ニャー>.
 
 =head1 AUTHOR
 
 azumakuniyuki E<lt>perl.org [at] azumakuniyuki.orgE<gt>
 
 =head1 SEE ALSO
+
 L<Acme::Nyaa>
 
 =head1 LICENSE
@@ -331,3 +365,4 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
